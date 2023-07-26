45487d901333f4366bdb27ca633a24f779e24532
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-07-26 17:07:10 +0200

    aggiustato codice e aggiunto vari commenti

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -26,18 +26,30 @@
             <input type="text" name="accessibility" id="accessibility">
             <br>
 
+            <!-- Dropdown per selezionare il tipo del progetto -->
             <select name="type_id" id="type_id">
+                <!-- Itera attraverso tutti i tipi disponibili -->
                 @foreach ($types as $type)
+                    <!-- Ogni opzione ha un valore corrispondente all'ID del tipo -->
                     <option value="{{ $type->id }}">
+                        <!-- Il testo all'interno dell'opzione è il nome del tipo -->
                         {{ $type->type_name }}
                     </option>
                 @endforeach
             </select>
             <br>
+            <!-- Checkbox per selezionare le tecnologie del progetto -->
             @foreach ($technologies as $technology)
-                <input type="checkbox" name="technologies[]" value="{{ $technology->id }}"> {{ $technology->name }}</input>
+                <!-- Ogni checkbox ha un nome (tecnologies[]) che indica un array delle tecnologie selezionate -->
+                <input type="checkbox" name="technologies[]" value="{{ $technology->id }}">
+                <!-- Il testo accanto alla checkbox è il nome della tecnologia -->
+                {{ $technology->name }}
+
+                </input>
             @endforeach
+            <!-- Bottone di submit per inviare il form -->
             <input class="my-3" type="submit" value="CREATE">
+
         </form>
     </div>
 @endsection
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -16,22 +16,32 @@
             <label for="description">description:</label>
             <input type="text" name="description" value="{{ $project->description }}" required>
 
-            <!-- Dropdown per i tipi -->
+            <!-- Dropdown per selezionare il tipo del progetto -->
+            <!-- Dropdown per selezionare il tipo del progetto -->
             <label for="type_id">Tipo:</label>
             <select name="type_id" required>
+                <!-- Itera attraverso tutti i tipi disponibili -->
                 @foreach ($types as $type)
-                    <option value="{{ $type->id }}" {{ $type->id == $project->type_id ? 'selected' : '' }}>
-                        {{ $type->type_name }}</option>
+                    <!-- Ogni opzione ha un valore corrispondente all'ID del tipo -->
+                    <option value="{{ $type->id }}" @if ($type->id == $project->type_id) selected @endif>
+                        <!-- Il testo all'interno dell'opzione è il nome del tipo -->
+                        {{ $type->type_name }}
+                    </option>
                 @endforeach
             </select>
 
-            <!-- Checkboxes per le tecnologie -->
+            <!-- Checkboxes per selezionare le tecnologie del progetto -->
             <label>Tecnologie:</label><br>
+            <!-- Itera attraverso tutte le tecnologie disponibili -->
             @foreach ($technologies as $technology)
+                <!-- Ogni checkbox ha un nome (tecnologies[]) che indica un array delle tecnologie selezionate -->
                 <input type="checkbox" name="technologies[]" value="{{ $technology->id }}"
-                    {{ in_array($technology->id, $project->technologies->pluck('id')->toArray()) ? 'checked' : '' }}>
+                    @if (in_array($technology->id, $project->technologies->pluck('id')->toArray())) checked @endif>
+                <!-- Il testo accanto alla checkbox è il nome della tecnologia -->
                 {{ $technology->name }}<br>
             @endforeach
+
+
 
             <button type="submit">Salva modifiche</button>
         </form>
